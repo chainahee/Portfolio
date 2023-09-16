8de5043329f1e6f3b4569664d5fc2bb142e69309
Supat1462 <102660629+Supat1462@users.noreply.github.com>
2023-09-16 16:32:26 +0700

    16092566

---
+++ script.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector("ul");
+let bars = document.querySelector(".header .fa-bars");
+
+bars.addEventListener("click", function () {
+  menu.classList.toggle("show");
+});
