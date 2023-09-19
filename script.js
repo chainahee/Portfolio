0dbab0a9926647a41bea15281ed2911888f12780
Supat1462 <102660629+Supat1462@users.noreply.github.com>
2023-09-19 18:01:07 +0700

    19/09/2023

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 let menu = document.querySelector("ul");
 let bars = document.querySelector(".header .fa-bars");
+let header = document.querySelector(".header");
 
 bars.addEventListener("click", function () {
   menu.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    header.classList.add("navbg");
+  } else {
+    header.classList.remove("navbg");
+  }
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener("DOMContentLoaded", () => {
+  const animatedImage = document.getElementById("animatedImage");
+
+  animatedImage.style.animationPlayState = "running"; // เริ่มการเคลื่อนไหว
 });
